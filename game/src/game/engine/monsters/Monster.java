7bebef5e3e4ef95ccd20f07eb3e75f2e073a51a0
Omar Mahmoud <omarmahmoudx3@gmail.com>
2026-03-08 10:49:10 +0200

    Add getters and setters to Monster class and create Dasher, Dynamo, MultiTasker, Schemer classes

--- game/src/game/engine/monsters/Monster.java
+++ game/src/game/engine/monsters/Monster.java
@@ -1,10 +1,12 @@
 package game.engine.monsters;
 import game.engine.Role;
+import game.engine.Constants;
+
 public abstract class Monster implements Comparable<Monster> {
-	String name;
-	String description;
+	String name; // READ ONLY
+	String description; // READ ONLY
 	Role role;
-	Role originalRole;
+	Role originalRole; // READ ONLY
 	int energy;
 	int position;
 	boolean frozen;
@@ -26,5 +28,70 @@ public abstract class Monster implements Comparable<Monster> {
 	// orders monsters based on who is more closer to the end
 	public int compareTo(Monster o) {
 		return this.position - o.position;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public Role getOriginalRole() {
+		return originalRole;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public int getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(int energy) {
+		if(energy >= Constants.MIN_ENERGY) {
+			this.energy = energy;
+		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		if(position >= Constants.STARTING_POSITION && position <= Constants.WINNING_POSITION) {
+			this.position = position;
+		}
+	}
+
+	public boolean isFrozen() {
+		return frozen;
+	}
+
+	public void setFrozen(boolean frozen) {
+		this.frozen = frozen;
+	}
+
+	public boolean isShielded() {
+		return shielded;
+	}
+
+	public void setShielded(boolean shielded) {
+		this.shielded = shielded;
+	}
+
+	public int getConfusionTurns() {
+		return confusionTurns;
+	}
+
+	public void setConfusionTurns(int confusionTurns) {
+		this.confusionTurns = confusionTurns;
 	}
 }
